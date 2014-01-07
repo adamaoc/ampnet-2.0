ba887abd2f675c3c09066442c27e0d1a646a0c29
adamaoc <adamaoc@gmail.com>
2014-01-07 07:07:54 -0600

    added and updating full work page

--- templates/about.php
+++ templates/about.php
@@ -20,7 +20,14 @@
 			</li>
 		</ul>
 		<h2>more about me</h2>
-		<p>I pride myself on being a family man. I love my family and they are number one in my book. I also pride myself in my work and work my hardest to produce things that I can be proud of.</p>
+		<div class="more-about">
+			<div class="img-wrap">
+				<img src="http://distilleryimage2.ak.instagram.com/7ae4dea876ea11e39c8f12a4e8421c6a_8.jpg" alt="Learn more about Adam and ampnet media" />
+			</div>
+			<div class="text-wrap">
+				<p>I pride myself on being a family man. I love my family and they are number one in my book. I also pride myself in my work and work my hardest to produce things that I can be proud of.</p>
+			</div>
+		</div>
 		<dl>
 			<dt>twitter:</dt>
 			<dd><a href="http://twitter.com/adamaoc">@adamaoc</a></dd>
