3367370462882038f98d6bd2aa97703cbc9cea1c
Adam Moore <adamaoc@gmail.com>
2014-05-03 09:53:48 -0500

    working on 404 page - not completed...

--- _flex/core/definitions.php
+++ _flex/core/definitions.php
@@ -19,6 +19,7 @@ define('IS_ABOUT', (bool)strstr($_SERVER['REQUEST_URI'], '/about/'));
 define('IS_WORK', (bool)strstr($_SERVER['REQUEST_URI'], '/work/'));
 define('IS_CONTACT', (bool)strstr($_SERVER['REQUEST_URI'], '/contact/'));
 define('IS_SUCCESS', (bool)strstr($_SERVER['REQUEST_URI'], '/success/'));
+// define('IS_404', (bool)strstr($_SERVER['REQUEST_URI'], '/404/'));
 
 define('IS_BLOG', (bool)strstr($_SERVER['REQUEST_URI'], '/blog/'));
 
@@ -33,6 +34,7 @@ $is_work = IS_WORK;
 $is_contact = IS_CONTACT;
 $is_success = IS_SUCCESS;
 $is_blog = IS_BLOG;
+// $is_404 = IS_404;
 
 // Define FLEX Classes // 
 define('FLEX_ROOT', $docPath);
---
+++ templates/404.php
@@ -0,0 +1,9 @@
+<section class="main-404">
+	<div class="container">
+		<h1>404 Not Found <small>ampnet<span>(media)</span></small></h1>
+		<blockquote>
+			Oops... Something just happened... I couldn't find the link you were looking for. Maybe head back to the <a href="/">home page</a> or the <a href="/blog/">blog</a> section and try again there.
+		</blockquote>
+		<h2>Thanks!</h2>
+	</div>
+</section>
