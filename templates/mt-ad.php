cd79f1b8a59a213739c026b91f2e8902ac77e096
Adam Moore <adamaoc@gmail.com>
2014-08-05 08:31:47 -0500

    Create mt-ad.php

---
+++ templates/mt-ad.php
@@ -0,0 +1,2 @@
+<?php ?>
+<a href="http://mediatemple.net#a_aid=512fd1cf8a22a&amp;a_bid=d1b49c1f" target="_top" rel="nofollow"><img src="https://affiliate.mediatemple.net/accounts/default1/banners/300x250-light.gif" alt="" width="300" height="250" /></a><img style="border:0" src="https://affiliate.mediatemple.net/scripts/imp.php?a_aid=512fd1cf8a22a&amp;a_bid=d1b49c1f" width="1" height="1" alt="" />
